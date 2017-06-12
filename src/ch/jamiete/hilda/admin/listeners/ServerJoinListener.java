da44a91560a51eb81068fb63c8db33e229ef7104
jamietech <jamie@jamiete.ch>
2017-06-12 20:37:18 +1000

    Update to EventHandler

--- src/ch/jamiete/hilda/admin/listeners/ServerJoinListener.java
+++ src/ch/jamiete/hilda/admin/listeners/ServerJoinListener.java
@@ -1,25 +1,25 @@
 package ch.jamiete.hilda.admin.listeners;
 
 import ch.jamiete.hilda.admin.AdminPlugin;
+import ch.jamiete.hilda.events.EventHandler;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-public class ServerJoinListener extends ListenerAdapter {
+public class ServerJoinListener {
     private final AdminPlugin plugin;
 
     public ServerJoinListener(final AdminPlugin plugin) {
         this.plugin = plugin;
     }
 
-    @Override
+    @EventHandler
     public void onGuildJoin(final GuildJoinEvent event) {
         if (this.plugin.getChannel() != null) {
             this.plugin.getChannel().sendMessage(":heavy_plus_sign: " + this.plugin.getServerInfo(event.getGuild())).queue();
         }
     }
 
-    @Override
+    @EventHandler
     public void onGuildLeave(final GuildLeaveEvent event) {
         if (this.plugin.getChannel() != null) {
             this.plugin.getChannel().sendMessage(":heavy_multiplication_x: " + this.plugin.getServerInfo(event.getGuild())).queue();
