41af0431b715036fa1f79d69219799e6be3713b1
jamietech <jamie@jamiete.ch>
2017-07-08 17:16:24 +1000

    Add exception command

--- src/ch/jamiete/hilda/admin/commands/AdminBaseCommand.java
+++ src/ch/jamiete/hilda/admin/commands/AdminBaseCommand.java
@@ -36,6 +36,7 @@ public class AdminBaseCommand extends ChannelSeniorCommand {
 
         this.registerSubcommand(new AdminBroadcastCommand(hilda, this));
         this.registerSubcommand(new AdminConfigCommand(hilda, this, plugin));
+        this.registerSubcommand(new AdminExceptionCommand(hilda, this));
         this.registerSubcommand(new AdminInfoCommand(hilda, this));
         this.registerSubcommand(new AdminInviteCommand(hilda, this));
         this.registerSubcommand(new AdminLeaveCommand(hilda, this));
