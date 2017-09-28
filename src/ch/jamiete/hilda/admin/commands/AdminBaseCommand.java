a821057ca2479e1b619d72812a36ac08b758cc89
jamietech <jamie@jamiete.ch>
2017-09-28 17:22:36 +1000

    Add restart command
    Terminates Hilda with a non-zero exit code to trigger a restart on the
    underlying shell script

--- src/ch/jamiete/hilda/admin/commands/AdminBaseCommand.java
+++ src/ch/jamiete/hilda/admin/commands/AdminBaseCommand.java
@@ -45,6 +45,7 @@ public class AdminBaseCommand extends ChannelSeniorCommand {
         this.registerSubcommand(new AdminMemoryCommand(hilda, this));
         this.registerSubcommand(new AdminMusicCommand(hilda, this, plugin));
         this.registerSubcommand(new AdminOverviewCommand(hilda, this));
+        this.registerSubcommand(new AdminRestartCommand(hilda, this));
         this.registerSubcommand(new AdminRolesCommand(hilda, this));
         this.registerSubcommand(new AdminSayCommand(hilda, this));
         this.registerSubcommand(new AdminServersCommand(hilda, this, plugin));
